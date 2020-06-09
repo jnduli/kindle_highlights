121419dc5b770b6c3e44756ff52f2b691604c70e
John Nduli <yohanaizraeli@gmail.com>
2020-06-09 21:54:32 +0300

    add support for id searching

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@ import qualified Data.List as L
 import qualified Data.Set as Set
 import System.IO  
 import           Text.Parsec
+import Text.Read
 import           Data.Time                      ( getCurrentTime )
 
 import CommandOptions
@@ -21,10 +22,23 @@ main = do
     let highlights = parse groups "fail" contents
     case highlights of
         Right a -> case inputType of
-                KindleList _ -> print (getUniqueBooks a)
-                KindleFilter s -> printKindleHighlights (filterByBookTitle s a)
+                KindleList _ -> printUniqueBooks (getUniqueBooks a)
+                KindleFilter s -> case readMaybe s :: Maybe Int of
+                    Just id -> printKindleHighlights (filterByBookId id a)
+                    Nothing -> printKindleHighlights (filterByBookTitle s a)
         Left _ -> print "fail"
     hClose handle
+
+printUniqueBooks :: Set.Set String -> IO ()
+printUniqueBooks xs = do
+    putStrLn "id\ttitle"
+    printZippedUniqueBook $zip [0..] (Set.toList xs)
+
+printZippedUniqueBook :: [(Int, String)] -> IO ()
+printZippedUniqueBook (x:xs) = do
+    putStrLn (show (fst x) ++ "\t" ++ snd x )
+    printZippedUniqueBook xs
+printZippedUniqueBook _ = return ()
 
 
 data KindleHighlight = KindleHighlight String String Integer Integer String deriving (Show, Eq)
@@ -38,7 +52,7 @@ getTitle (KindleHighlight title _ _ _ _) = title
 
 getUniqueBooks :: [KindleHighlight] -> Set.Set String
 getUniqueBooks [] = Set.empty
-getUniqueBooks xs = Set.fromList [getTitle x | x <- xs]
+getUniqueBooks xs = Set.fromList $L.sort [getTitle x | x <- xs]
 
 -- TODO
 -- change ordering
@@ -72,6 +86,10 @@ printKindleHighlights (x:xs) = do
     putStrLn (getContent x ++ "\n\n")
     printKindleHighlights xs
 printKindleHighlights _ = print "\n\n"
+
+filterByBookId :: Int -> [KindleHighlight] -> [KindleHighlight]
+filterByBookId id xs = filterByBookTitle (Set.elemAt id (getUniqueBooks xs)) xs
+
 
 filterByBookTitle :: String -> [KindleHighlight] -> [KindleHighlight]
 filterByBookTitle title = filter (checkBookTitle title)
